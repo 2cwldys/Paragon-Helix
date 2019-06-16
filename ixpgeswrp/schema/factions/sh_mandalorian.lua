fe901a46aaf3dd8981a04bfcc1bbb3a3f542a4fa
Asimo1234 <33825053+Asimo1234@users.noreply.github.com>
2019-06-16 20:21:02 +0200

    Models for Mando

--- ixpgeswrp/schema/factions/sh_mandalorian.lua
+++ ixpgeswrp/schema/factions/sh_mandalorian.lua
@@ -2,6 +2,12 @@ FACTION.name = "Mandalorian"
 FACTION.description = "A fearful and menacing bounty hunter, their profession is made very clear to the known galaxy through a rocky history of collective legend and culture."
 FACTION.color = Color(155, 41, 232, 255)
 FACTION.isGloballyRecognized = true
+FACTION.models = {
+	"models/smitty/swbf/sm_variant_1/hero_gunslinger_sm_variant_1.mdl",
+	"models/smitty/swbf/sm_variant_2/hero_gunslinger_sm_variant_2.mdl",
+	"models/smitty/swbf/sm_variant_3/hero_gunslinger_sm_variant_3.mdl",
+	"models/smitty/swbf/sm_variant_4/hero_gunslinger_sm_variant_4.mdl"
+}
 FACTION.isDefault = false
 FACTION.runSounds = {[0] = "NPC_CombineS.RunFootstepLeft", [1] = "NPC_CombineS.RunFootstepRight"}
 
